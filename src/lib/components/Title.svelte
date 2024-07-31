a7778cad364de5be2dd2e437f450e3f0edde08b7
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-07-31 11:10:04 -0400

    Initialize SvelteKit project with title component

---
+++ src/lib/components/Title.svelte
@@ -0,0 +1,9 @@
+<script>
+    // https://sveltebyexample.com/components-and-props/
+    export let titleContent = 'TITLE';
+</script>
+
+<!-- https://tailwindcss.com/docs/responsive-design#targeting-mobile-screens -->
+<div class="text-center text-xl lg:text-4xl font-serif"> 
+<h1>{titleContent}</h1>
+</div>
