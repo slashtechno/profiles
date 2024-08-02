adb03acee755592a29fd9cb3b2a1efb6fee124e5
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-08-02 17:30:45 -0400

    feat: fetch user on the client

--- src/routes/users/[user]/+page.js
+++ src/routes/users/[user]/+page.js
@@ -1,5 +1,8 @@
-export function load({ params }) {
+// THIS RUNS ON THE SERVER!
+export async function load({ params }) {
   // console.log("load", params);
+  // https://github.com/pocketbase/pocketbase/discussions/3677#discussioncomment-7477178
+
   return {
     username: params.user,
   };
--- src/routes/users/[user]/+page.svelte
+++ src/routes/users/[user]/+page.svelte
@@ -1,6 +1,18 @@
 <script>
     export let data;
+    
+    import { pb } from "$lib/pocketbase";
+    import { onMount } from 'svelte';
+
     let username = data.username;
+    let record
+    onMount(async () => {
+        record = await pb.collection("users").getFirstListItem(
+            pb.filter("username={:username}", { username: username })
+        );
+        console.log(record);
+    });
+    
 </script>
 
 <h3 class="text-2xl text-center">{username}</h3>
