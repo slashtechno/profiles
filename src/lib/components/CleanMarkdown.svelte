121981a5e8c996ca1c1df112d38361d0e152c9d8
slashtechno <77907286+slashtechno@users.noreply.github.com>
2024-08-16 20:32:54 -0400

    fix: Style headings in Markdown feat: Improve project info

--- src/lib/components/CleanMarkdown.svelte
+++ src/lib/components/CleanMarkdown.svelte
@@ -1,5 +1,6 @@
 <script>
-    export let src;
+    export let src = "Nothing to see here";
+    // let src = "## abcxyz"; content
     import insane from "insane";
     import SvelteMarkdown from "svelte-markdown";
     import { onMount } from 'svelte';
@@ -18,4 +19,7 @@
     });
 </script>
 
+<div class="markdown">
+<!-- <SvelteMarkdown source={src} /> -->
 <SvelteMarkdown source={insane(src, allowedConfig)} />
+</div>
--- src/lib/components/Info.svelte
+++ src/lib/components/Info.svelte
@@ -1,35 +1,46 @@
 <script lang="ts">
-    import {
-      Button,
-      buttonVariants
-    } from "$lib/components/ui/button/index.js";
-    import * as Dialog from "$lib/components/ui/dialog/index.js";
-    import { Input } from "$lib/components/ui/input/index.js";
-    import { Label } from "$lib/components/ui/label/index.js";
-  </script>
-   
-  <Dialog.Root>
-    <Dialog.Trigger class={buttonVariants({ variant: "outline" })}
+  import { Button, buttonVariants } from "$lib/components/ui/button/index.js";
+  import * as Dialog from "$lib/components/ui/dialog/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+</script>
+
+<div class="mb-4">
+<Dialog.Root>
+  <div class="flex justify-center">
+    <Dialog.Trigger class={`${buttonVariants({ variant: "outline" })}`}
       >What's this?</Dialog.Trigger
     >
-    <Dialog.Content class="sm:max-w-[425px]">
-      <Dialog.Header>
-        <Dialog.Title>What's this?</Dialog.Title>
+  </div>
+  <Dialog.Content class="sm:max-w-[425px]">
+    <Dialog.Header>
+      <Dialog.Title>What's this?</Dialog.Title>
     </Dialog.Header>
-        <Dialog.Description>
-          Social media platforms can be bloated. What about a rich text landing page?
-        </Dialog.Description>
-        <!-- Center -->
-        <div class="flex justify-center">
-            <!-- TODO: Improve margin/padding -->
-        <p>
-          profiles is a project that allows you to make a home for yourself on the internet with markdown. Each user gets their own profile page, which they can customize with markdown. Why markdown? It's simple, easy to learn, and can be used to create rich text.
-        <Button href="https://github.com/slashtechno/profiles" target="_blank">
-            View on GitHub (and maybe star it?)
-            </Button>
-      </div>
-      <Dialog.Footer>
-        <Button type="submit">Save changes</Button>
-      </Dialog.Footer>
-    </Dialog.Content>
-  </Dialog.Root>
+    <Dialog.Description>
+      Social media platforms can be bloated. What about a rich text landing page that's yours to customize?
+    </Dialog.Description>
+    <!-- Center -->
+    <div class="flex justify-center flex-col items-center">
+      <p>
+        <em>profiles</em> is a project that allows you to make a home for yourself
+        on the internet with markdown. Each user gets their own profile page, which
+        can be customized with markdown. Why markdown? It's simple, easy to learn,
+        and can be used to create rich text. In addition, things like images or even iframes can be embedded.
+      </p>
+      <br>
+      <p>If you haven't used markdown before, now's a perfect time to <a href="https://www.markdownguide.org/cheat-sheet/" target="_blank">learn</a> about it! Before it's rendered, the markdown is sanitized.</p>
+      <br>
+      <p>Each profile has it's own unique URL, which can be shared with others. There are no ads and no magic algorithms. Just you and your home on the internet.</p>
+      <br>
+      <p>The code is open source and contributions are welcome!</p>
+    </div>
+    <Button href="https://github.com/slashtechno/profiles" target="_blank">
+      View on GitHub (and maybe star it?)
+    </Button>
+    <Dialog.Footer>
+      <!-- <Button type="submit">Save changes</Button> -->
+      <p>- Angad Behl</p>
+    </Dialog.Footer>
+  </Dialog.Content>
+</Dialog.Root>
+</div>
